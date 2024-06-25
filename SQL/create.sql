0544e42e3edf9c18fdbc476b86f2557616d8c983
MarkEwert03 <mewert03@gmail.com>
2024-06-25 16:08:06 -0700

    created ER diagram to model project and started filling out create.sql

---
+++ SQL/create.sql
@@ -0,0 +1,32 @@
+CREATE TABLE User (
+    user_id INTEGER PRIMARY KEY,
+    user_name TEXT NOT NULL,
+    age INTEGER,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Catalogue (
+    cat_id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    cat_name TEXT NOT NULL,
+    cat_description TEXT,
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+CREATE TABLE Game (
+    game_id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    console TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    release_date DATE,
+);
+
+CREATE TABLE CatalogueGame (
+    cat_id INTEGER,
+    game_id INTEGER,
+    playtime INTEGER,
+    notes TEXT,
+    PRIMARY KEY (cat_id, game_id),
+    FOREIGN KEY (cat_id) REFERENCES Catalogue(cat_id),
+    FOREIGN KEY (game_id) REFERENCES Game(game_id)
+);
